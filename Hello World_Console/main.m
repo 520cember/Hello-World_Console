277646430c97823f048dfe2100baa35970ab6694
520cember <1029278668@qq.com>
2016-01-13 22:52:51 +0800

    Initial Commit

---
+++ Hello World_Console/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello World_Console
+//
+//  Created by 董兴斌 on 16/1/13.
+//  Copyright © 2016年 董兴斌. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
